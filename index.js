a738a09b314d9c75ea4905313252d94265708926
DrVonDevious <drvondevious@gmail.com>
2020-03-18 15:14:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Contutorialuctor OO pattern?',
@@ -12,5 +13,7 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(s => s.split(' ').map( w => w.substring(0,1).toUpperCase() + w.substring(1)).join(' '))
 }
+
+console.log(titleCased())
